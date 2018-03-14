d2b92c6b90a03aa1f219b2e91067bffe6ec0fde9
Handy.Jacob <Handy.Jacob@SEMS-259-33.SEMS259.LAB>
2018-03-14 14:56:32 -0700

    Pi Aloud and Change Calculator

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -2,6 +2,7 @@
 //    Level 0
 
 
+import javax.print.attribute.standard.JobKOctetsProcessed;
 import javax.swing.JOptionPane;
 
 /*
@@ -14,17 +15,26 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String nickels = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+int Nickels = Integer.parseInt(nickels);
+Nickels = Nickels * 5;
 		// Ask the user how many dimes they have, and convert their answer
-
+String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
 		// Ask the user how many quarters they have, and convert their answer
-
+int Dimes = Integer.parseInt(dimes);
+Dimes = Dimes * 10;
+		String quarters = JOptionPane.showInputDialog("How many quarters do you have?");
+Integer.parseUnsignedInt(quarters);
+int Quarters = Integer.parseInt(quarters);
+Quarters = Quarters * 25;
 		// Calculate how much money the user has and save it in a double variable 
-
+int total = Quarters + Dimes + Nickels;
 		// Tell the user how much money they have
-
+if(total>=100) {
+	total = total/100;
+}
+JOptionPane.showMessageDialog(null, "Your balance is " + total + "dollars");
 	}
 }
 
--- src/PiAloud.java
+++ src/PiAloud.java
@@ -7,9 +7,17 @@ import java.util.Scanner;
 
 public class PiAloud {
 
-	// 1. Make a main method and make sure your program can run
-
-	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
+	// 1done. Make a main method and make sure your program can run
+public static void main(String[] args) {
+	String Pi = "3.14159265358979323846264338327950288419716939937510582";
+    int digit = -1;
+	for(int x = 0; x<55; x++) {
+		digit++;
+		System.out.println(Pi.charAt(digit));
+	}
+	
+}
+	// 2done. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
 
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 
